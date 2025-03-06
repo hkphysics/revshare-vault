704cf8256f4f63aef7268ab76ffd770751ff90d6
Joseph C Wang <joequant@gmail.com>
2025-03-06 19:21:02 +0000

    add natspec

--- contracts/RevShare.sol
+++ contracts/RevShare.sol
@@ -8,6 +8,8 @@ import { AccessControl } from "@openzeppelin/contracts/access/AccessControl.sol"
 
 
 /**
+ * @title RevShareToken
+ * @dev This contract distributes revenue to token holders using a modified MasterChef algorithm.
 
 This is a RevShare contract that distributes revenue between token holders.
 
@@ -32,10 +34,6 @@ The total number of tokens which a user is entitied to is
 The number of tokens to be claimed is
 
    (user_tokens_claimed) = (total_user_tokens) - (user_tokens_received)
-
-*/
-
-/**
 
 To distribute tokens, the user is checked against a white list if the user
 passes, then the smart contract will check how many tokens are available in
@@ -64,11 +62,22 @@ contract RevShareToken is ERC20, AccessControl {
     mapping(address => UserPool) public userPool;
     TotalPool public totalPool;
 
+    /**
+     * @dev Constructor to initialize the contract and set the token address.
+     * @param token_ Address of the token to be used for distribution.
+     */
     constructor(address token_) ERC20("RevShare", "RBF") {
 	token = IERC20(token_);
 	_grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
+    /**
+     * @dev Mints new tokens to a specified address.
+     * @param to Address to which the tokens will be minted.
+     * @param amount Amount of tokens to mint.
+     * Requirements:
+     * - Caller must have the MINTER_ROLE.
+     */
     function mint(address to, uint256 amount) public {
         if (!hasRole(MINTER_ROLE, msg.sender)) {
 	    revert AccessFailed();
@@ -76,6 +85,13 @@ contract RevShareToken is ERC20, AccessControl {
         _mint(to, amount);
     }
 
+    /**
+     * @dev Burns tokens from a specified address.
+     * @param from Address from which the tokens will be burned.
+     * @param amount Amount of tokens to burn.
+     * Requirements:
+     * - Caller must have the BURNER_ROLE.
+     */
     function burn(address from, uint256 amount) public {
         if (!hasRole(BURNER_ROLE, msg.sender)) {
 	    revert AccessFailed();
@@ -83,13 +99,23 @@ contract RevShareToken is ERC20, AccessControl {
         _burn(from, amount);
     }
 
+    /**
+     * @dev Internal function to update user pools when tokens are transferred.
+     * @param from Address from which tokens are being transferred.
+     * @param to Address to which tokens are being transferred.
+     * @param value Amount of tokens being transferred.
+     */
     function _update(address from,  address to, uint256 value)  internal virtual override {
 	super._update(from,  to,  value);
 	_updateUserPool(from);
 	_updateUserPool(to);
     }
 
-    function _updateUserPool(address addr) internal virtual {
+    /**
+     * @dev Internal function to update the user pool for a specific address.
+     * @param addr Address of the user whose pool is being updated.
+     */
+    function _updateUserPool(address addr) internal {
 	if (addr == address(0)) {
 	    return;
 	}
@@ -99,6 +125,12 @@ contract RevShareToken is ERC20, AccessControl {
 	userPool[addr].lastCalc = totalPool.tokensDistributed;
     }
 
+    /**
+     * @dev Distributes a specified amount of tokens to the total pool.
+     * @param amount Amount of tokens to distribute.
+     * Requirements:
+     * - Caller must have the DISTRIBUTE_ROLE.
+     */
     function distribute(uint256 amount) public {
 	if (!hasRole(DISTRIBUTE_ROLE, msg.sender)) {
 	    revert AccessFailed();
@@ -107,6 +139,11 @@ contract RevShareToken is ERC20, AccessControl {
 	totalPool.weightedAverage += totalSupply() * amount;
     }
 
+    /**
+     * @dev Allows users to claim their tokens based on their share.
+     * Requirements:
+     * - Caller must have the CLAIM_ROLE.
+     */
     function claim() public {
         if (!hasRole(CLAIM_ROLE, msg.sender)) {
 	    revert AccessFailed();
